122f80aee7c9190824327dec1637f4d854922280
mate.magyari <mate.magyari@gmail.com>
2014-10-17 15:30:44 +0200

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mars-rovers
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject mars-rovers "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src"]
+  :test-paths   ["test"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]]
+  :main marsrovers.mars-expedition-runner)
